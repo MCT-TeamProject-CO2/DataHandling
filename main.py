d0f8894156b84e22638c3ff6fa5f1ee4f14627e1
kilian okladnicoff <kiokl2000@hotmail.com>
2021-01-06 02:23:46 +0100

    added comments to code

--- main.py
+++ main.py
@@ -5,30 +5,42 @@ import threading
 import datetime
 import json
 
+# on message function to execute when a message
+# comes in on the broker
 def on_message(client, userdata, msg):
     data = json.loads((msg.payload).decode('utf-8'))
 
 #region DEBUG
+# some code only for debugging, as it slows down the service a big chunk
     # now = datetime.datetime.now()
     # print(f"written data on {now.strftime('%b %d %Y %H:%M:%S')}: {str(data)}")
 #endregion
 
+    # create a data point for the influx database from the received mqtt data
     point = Point(data["Room"])\
         .field("humidity", float(data["Humidity"]))\
         .field("tvoc_ppb", float(data["tvoc_ppb"]))\
         .field("temperature", float(data["Temperature"]))\
         .field("co2eq_ppm", float(data["co2eq_ppm"]))\
         .time(data["Timestamp"], WritePrecision.NS)
+    # write the data to the database
     write_api.write(bucket, organization, point)
 
 if __name__ == "__main__":
     try:
+        # set the on message function for the mqtt client
         client.on_message = on_message
+        
+        # connect to the client and sunscribe to the topic
         client.connect(host_address, topic, keep_alive=60)
+        
+        # keep looping and keep reading data from the broker
         client.loop_forever(timeout=0.5)
     except KeyboardInterrupt:
+        # if manually stopped through a keyboardinterupt, give general message
         print("\nManually stopped")
     except Exception as e:
+        # else show error
         print(f"Error: {e}")
     finally:
         client.disconnect()
--- models/mqtt.py
+++ models/mqtt.py
@@ -1,14 +1,19 @@
 import paho.mqtt.client as paho
 import threading
 
+# this class extends the basic mqtt Client class from the paho module
 class MyClient(paho.Client):
+    # initialize the paho mqtt client on intantiating this class
     def __init__(self):
         super().__init__()
     
+    # check a given parameter against the expected type, and return an error if not the expected type
     @staticmethod
     def __check_type(given, expected):
         return given if isinstance(given, expected) else AttributeError(f'AttributeError: Excpected type {expected}, given type {type(given)}')  
     
+    # custom on connect with basic info display
+    # also subscribes to the topic, so it does not need to be done in the main.py file
     def __on_connect(self, client, flags, userdata, rc):
         print(f'''
             client connected:
@@ -17,6 +22,7 @@ class MyClient(paho.Client):
                 userdata > {userdata}''')
         client.subscribe(self.__topic)
     
+    # general connect method to initialize an mqtt connection
     def connect(self, hostaddr:str, topic:str, port:int=1883, keep_alive:int=0):
         self.__topic = MyClient.__check_type(topic, str)
         MyClient.__check_type(hostaddr, str)
